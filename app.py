4cbcd8e7831f6e1f193ef5162ff0b30e5fcf70bd
Howard HC Huang <howard0926@hotmail.com>
2021-08-29 02:02:33 +0800

    add app.py

---
+++ app.py
@@ -0,0 +1,52 @@
+#我們在寫一個伺服器,以下程式碼就是在架設一個伺服器
+#我們在做一個web app時通常會把伺服器的主要檔案取為app.py
+#python架伺服器(or寫網站)最主流兩種套件:flask, django
+#代表這個程式碼是用flask寫的,是用來架設伺服器的
+#承上(代表今天就算是一個軟體工程師,只要他不是走網頁領域的話,他不見得懂這個程式碼,即使他很熟python)
+
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
+
+app = Flask(__name__)
+
+line_bot_api = LineBotApi('token')
+handler = WebhookHandler('secret') #secret
+
+
+@app.route("/callback", methods=['POST'])    #www.line-bot.com"/callback"有人來這個路徑(callboack結尾)敲門的話,就會執行這個觸發事件
+def callback():    #返回的觸發事件    #所以我們要在MessagingAPI裡輸入我們程式放在雲端的網址,line就會把訊息轉載到我們給他的網址
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        print("Invalid signature. Please check your channel access token/channel secret.")
+        abort(400)
+
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessage)    #上面那個func會觸發這個handleFunction,顧名思義就是在處理訊息,使用者傳來的訊息有某某某,然後我們就做什麼動作
+def handle_message(event):
+    line_bot_api.reply_message(    #回覆訊息
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
+
+
+if __name__ == "__main__": #寫這行是為:程式"被執行"才執行,而不是"被import"就執行,否則才剛寫random,電腦就在產生亂數,CPU就跑很高,這樣不好
+    app.run()
